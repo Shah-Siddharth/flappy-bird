27863481e2cf0d0469afb0d2a2bfae0c3840e2aa
Siddharth <siddharthshah1102@gmail.com>
2023-03-04 18:14:35 +0530

    scale using push, background images with infinite scroll

---
+++ components/BackgroundImage.lua
@@ -0,0 +1,28 @@
+BackgroundImage = {}
+BackgroundImage.__index = BackgroundImage
+
+function BackgroundImage:init(imagePath, x, y, scrollSpeed)
+    local this = {}     --'this' is a new instance
+    setmetatable(this, BackgroundImage)
+    
+    this.img = love.graphics.newImage(imagePath)
+    this.x = x
+    this.y = y
+    this.scrollSpeed = scrollSpeed
+    
+    return this
+end
+
+function BackgroundImage:update(dt)
+    if self.x + self.img:getWidth() <= 0 then
+        self.x = 0
+    end
+
+    self.x = self.x - (self.scrollSpeed * dt)
+    print(self.x, love.graphics.getWidth())
+end
+
+function BackgroundImage:draw()
+    love.graphics.draw(self.img, self.x, self.y)
+    love.graphics.draw(self.img, self.x + self.img:getWidth(), self.y)
+end
--- main.lua
+++ main.lua
@@ -1,25 +1,34 @@
-local background = love.graphics.newImage('parallax-forest-back-trees.png')
+require("components.BackgroundImage")
 
-function love.conf(t)
-    t.console = true
-end
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+local sx = WINDOW_WIDTH / VIRTUAL_WIDTH
+local sy = WINDOW_HEIGHT / VIRTUAL_HEIGHT
+
+local bgLayer1 = BackgroundImage:init('assets/background-layer-1.png', 0, 0, 30)
+local bgLayer2 = BackgroundImage:init('assets/background-layer-2.png', 0, 0, 45)
 
 function love.load()
-    love.window.setMode(1024, 576, {
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
-        vsync = true
+        vsync = 1
     })
-    
+
     love.window.setTitle('Flappy Bird')
 end
 
 function love.update(dt)
-    scaleX = love.graphics.getWidth() / background:getWidth()
-    scaleY = love.graphics.getHeight() / background:getHeight()
-    print(scaleX)
+    bgLayer1:update(dt)
+    bgLayer2:update(dt)
 end
 
 function love.draw()
-    love.graphics.draw(background, 0, 0, 0, scaleX, scaleY)
+    love.graphics.scale(sx, sy)
+    bgLayer1:draw()
+    bgLayer2:draw()
 end
